c5957b99729e0b0f9053b9a9f0da0cf05682d509
valeriy <portmone1@gmail.com>
2026-04-05 16:28:22 +0300

    timer

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const delay = Number(formData.get('delay'));
+  const state = formData.get('state');
+
+  createPromise(delay, state)
+    .then(value => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${value}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(value => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${value}ms`,
+        position: 'topRight',
+      });
+    });
+
+  event.currentTarget.reset();
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
